b3e278c69c6515bea9c532aa167a5e5061b165a6
lFGrahaml <Fgraham1@asu.edu>
2024-11-27 22:52:55 -0700

    removed model reference

--- application/routes.py
+++ application/routes.py
@@ -1,9 +1,7 @@
 from flask import render_template
 #from . import db
-#from .models import User
 from flask import current_app as app
 
 @app.route('/')
 def index():
-    #users = User.query.all()
     return render_template('home.html')
