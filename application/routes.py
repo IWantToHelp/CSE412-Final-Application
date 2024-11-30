341ce431a7129d39a2d147af4707c564aa416cc7
lFGrahaml <Fgraham1@asu.edu>
2024-11-29 19:43:38 -0700

    Created Models for Pokemon Database + Made a route that displays all trainer's names and ids to test fucntionality

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,15 @@
-from flask import render_template
-#from . import db
+from flask import render_template, Flask, jsonify
+from . import db
+from .models import Trainer
 from flask import current_app as app
 
+# Home Page
 @app.route('/')
 def index():
     return render_template('home.html')
+
+# Temporary Route that outputs all the trainers IDs and names that are in database
+@app.route('/data')
+def get_data():
+    data = Trainer.query.all()
+    return jsonify([{"trainer id": row.tr_trainerid, "trainer name": row.tr_name} for row in data])
