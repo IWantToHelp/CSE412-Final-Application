a90834e7afbce8b4356d191821558d2a8fe09bd6
Tyler Nguyen <38928951+IWantToHelp@users.noreply.github.com>
2024-12-06 16:31:59 -0700

    Added all the routes needed for each button added for the html

--- application/routes.py
+++ application/routes.py
@@ -3,8 +3,9 @@ from . import db
 from .models import *
 from flask import current_app as app
 from sqlalchemy import create_engine, text
+from config import Config
 
-# engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
+engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
 # Home Page
 @app.route('/')
 def index():
@@ -109,6 +110,100 @@ def get_nature():
     # return render_template('home.html', data=trainerData)
     return render_template('home.html', data=natureData, columns = natureColumns, table_name=table_name)
     # return jsonify([{"trainer id": row.tr_trainerid, "trainer_name": row.tr_name} for row in data])
+
+@app.route('/immunity')
+def get_immunity():
+    table_name="Immunity"
+    try:
+        with engine.connect() as conn:
+            result = conn.execute(text(f"SELECT a.t_name AS Type, b.t_name AS TakesNoDamageFrom FROM TYPES as a, TYPES as b, immune WHERE i_effectiveid = a.t_typeid AND i_typeid = b.t_typeid;"))
+            data = [row for row in result]  # Row data
+            columns = result.keys()  # Column names
+    except Exception as e:
+        # Handle errors (e.g., table not found)
+        return f"An error occurred: {str(e)}"
+    natureColumns = ["Type", "Takes No Damage From"]
+
+    return render_template('home.html', data=data, columns = natureColumns, table_name=table_name)
+   
+@app.route('/neutral')
+def get_neutral():
+    table_name="Neutral"
+    try:
+        with engine.connect() as conn:
+            result = conn.execute(text(f"SELECT a.t_name AS Type, b.t_name AS TakesNoDamageFrom FROM TYPES as a, TYPES as b, neutraleffective WHERE ne_effectiveid = a.t_typeid AND ne_typeid = b.t_typeid;"))
+            data = [row for row in result]  # Row data
+            columns = result.keys()  # Column names
+    except Exception as e:
+        # Handle errors (e.g., table not found)
+        return f"An error occurred: {str(e)}"
+    natureColumns = ["Type", "Takes Neutral Damage From"]
+
+    return render_template('home.html', data=data, columns = natureColumns, table_name=table_name)
+
+@app.route('/super')
+def get_super():
+    table_name="Super Effective"
+    try:
+        with engine.connect() as conn:
+            result = conn.execute(text(f"SELECT a.t_name AS Type, b.t_name AS TakesNoDamageFrom FROM TYPES as a, TYPES as b, supereffective WHERE se_effectiveid = a.t_typeid AND se_typeid = b.t_typeid;"))
+            data = [row for row in result]  # Row data
+            columns = result.keys()  # Column names
+    except Exception as e:
+        # Handle errors (e.g., table not found)
+        return f"An error occurred: {str(e)}"
+    natureColumns = ["Type", "Supereffective Against"]
+
+    return render_template('home.html', data=data, columns = natureColumns, table_name=table_name)
+
+@app.route('/not')
+def get_not():
+    table_name="Not Very Effective"
+    try:
+        with engine.connect() as conn:
+            result = conn.execute(text(f"SELECT a.t_name AS Type, b.t_name AS TakesNoDamageFrom FROM TYPES as a, TYPES as b, notveryeffective WHERE nve_effectiveid = a.t_typeid AND nve_typeid = b.t_typeid;"))
+            data = [row for row in result]  # Row data
+            columns = result.keys()  # Column names
+    except Exception as e:
+        # Handle errors (e.g., table not found)
+        return f"An error occurred: {str(e)}"
+    natureColumns = ["Type", "Not Very Effective Against"]
+
+    return render_template('home.html', data=data, columns = natureColumns, table_name=table_name)
+
+@app.route('/pokemon')
+def get_pokemon():
+    table_name="Pokemon"
+    try:
+        with engine.connect() as conn:
+            result = conn.execute(text(f"""SELECT p_name AS Pokemon, a.t_name AS Type1, b.t_name AS Type2, p_gender AS Gender, p_level AS Level, p_pokedexid AS PokedexID, tr_name AS Trainer, n_name as Nature
+FROM pokemon, trainer, nature, typecombo, types as a, types as b
+WHERE tr_trainerid=p_trainerid AND p_natureid=n_natureid AND p_typescombo=tc_id AND tc_firsttype = a.t_typeid AND (tc_secondtype = b.t_typeid OR tc_secondtype = NULL);
+"""))
+            data = [row for row in result]  # Row data
+            columns = result.keys()  # Column names
+    except Exception as e:
+        # Handle errors (e.g., table not found)
+        return f"An error occurred: {str(e)}"
+
+    return render_template('home.html', data=data, columns = columns, table_name=table_name)
+
+@app.route('/trainer')
+def get_trainer():
+    table_name="Trainer"
+    try:
+        with engine.connect() as conn:
+            result = conn.execute(text(f"""SELECT tr_name AS name, c_classname AS class, tr_money_given AS money_given
+FROM trainer, class
+WHERE tr_classid=c_classid;
+"""))
+            data = [row for row in result]  # Row data
+            columns = result.keys()  # Column names
+    except Exception as e:
+        # Handle errors (e.g., table not found)
+        return f"An error occurred: {str(e)}"
+
+    return render_template('home.html', data=data, columns = columns, table_name=table_name)
 
 
 #Route that allows users to add trainers (but just their trainer ID and name) to the database; expand this to add entire entries
