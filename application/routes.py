ac15c84d719f6b7d6e17775730c5650210c412e6
Tyler Nguyen <38928951+IWantToHelp@users.noreply.github.com>
2024-12-05 23:00:01 -0700

    Pushing changes so far, localhost/data will show trainer data

--- application/routes.py
+++ application/routes.py
@@ -2,16 +2,54 @@ from flask import render_template, Flask, jsonify
 from . import db
 from .models import Trainer
 from flask import current_app as app
+from sqlalchemy import create_engine, text
 
+# engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
 # Home Page
 @app.route('/')
 def index():
     return render_template('home.html')
 
+# app = Flask(__name__)
+## sample code to dynamically show table names
+# # Database connection URL (update with your credentials)
+# DB_URI = "postgresql+psycopg2://username:password@localhost:5432/your_database_name"
+
+# # Create SQLAlchemy engine
+# engine = create_engine(DB_URI)
+
+# @app.route('/<table_name>')
+# def display_table(table_name):
+#     # Query the database dynamically based on the table name
+#     try:
+#         with engine.connect() as conn:
+#             result = conn.execute(text(f"SELECT * FROM {table_name}"))
+#             data = [row for row in result]  # Row data
+#             columns = result.keys()  # Column names
+#     except Exception as e:
+#         # Handle errors (e.g., table not found)
+#         return f"An error occurred: {str(e)}"
+
+#     return render_template('table.html', table_name=table_name, columns=columns, data=data)
+
 # Temporary Route that outputs all the trainers IDs and names that are in database
 @app.route('/data')
 def get_data():
+    table_name="Trainers"
     data = Trainer.query.all()
+    trainerColumns = ["Trainer ID", "Name", "Class", "Money Given"]
+
+    # with engine.connect() as conn:
+    #     result = conn.execute(text("SELECT * FROM Trainer"))
+    # data1 = [row for row in data]  # Convert result to a list of rows
+    trainerData = []
+    for row in data:
+        trainerData.append([row.tr_trainerid, row.tr_name, row.tr_classid, row.tr_money_given])
+    print(data)
+    print(trainerData)
+    # columns = data.keys()
+    # return render_template('home.html', data=trainerData)
+    return render_template('home.html', data=trainerData, columns = trainerColumns, table_name=table_name)
     return jsonify([{"trainer id": row.tr_trainerid, "trainer_name": row.tr_name} for row in data])
 
 # ADD CODE BELOW
